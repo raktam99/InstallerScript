30aeda1a2b067005afb3cfd1f44688eafa7a767e
raktam99 <raktam78@gmail.com>
2023-09-14 22:59:39 +0200

    Small stuff

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
+import os
 import tkinter as tk
 from tkinter import Checkbutton, IntVar
 from PIL import Image
 from PIL import ImageTk
 import requests
 import subprocess
-
 import programs
+import time
 
 
-#Downloader
+# Downloader function
 def download_app(application):
+    print(f"Downloading {application.name}...")
     try:
         response = requests.get(application.url)
         response.raise_for_status()  # Check for HTTP errors
@@ -21,81 +23,93 @@ def download_app(application):
     except Exception as e:
         print(f"Error: {str(e)}")
 
-    run_installer(application.path, application.name, application.flags)
 
-#Install app
-def run_installer(path, name, flags):
+# Installer function
+def install_app(application):
     try:
-        cmd = [path] + flags
-        print(f"Installing {name}...")
+        cmd = [application.path] + application.flags
+        print(f"Installing {application.name}...")
         subprocess.run(cmd, shell=True, check=True)
     except subprocess.CalledProcessError as e:
-        print(f"Error installing {name}: {e}")
+        print(f"Error installing {application.name}: {e}")
     except FileNotFoundError:
-        print(f"Installer not found at path: {path}")
+        print(f"Installer not found at path: {application.path}")
 
-#Click handler
+
+# Click handler function
 def bttn_click():
     if chrome_invar.get() == 1:
-        print("Downloading Chrome...")
-        download_app(programs.chrome)
+        programs_to_install.append(programs.chrome)
 
     if opera_invar.get() == 1:
-        print("Downloading Opera...")
-        download_app(programs.opera)
+        programs_to_install.append(programs.opera)
 
-    print("Everything is done!")
+    for program in programs_to_install:
+        download_app(program)
+        install_app(program)
 
-#Main app window
+    print("Everything is installed!")
+    time.sleep(2)
+    print("Removing installers...")
+
+    #for program in programs_to_install:
+        #os.remove(program.path)
+
+    print("Done!")
+
+    quit()
+
+
+# List for programs that will be installed
+programs_to_install = []
+
+
+# Main app window
 root = tk.Tk()
 root.title("Installer")
 
-#Text
+# Text
 choose_label = tk.Label(root, text="Please choose!")
 choose_label.grid(row=0, column=0, padx=2, pady=2, columnspan=2)
 
+# Chrome
 
-#Chrome
 
-
-#Load image
+# Load image
 chrome_image = Image.open("./Resources/chrome.png")  # Replace "example.png" with your image file path
 chrome_image = chrome_image.resize((20, 20))  # Resize the image to your desired dimensions
 chrome_photo = ImageTk.PhotoImage(chrome_image)
 
-#Display image
+# Display image
 chrome_image_label = tk.Label(root, image=chrome_photo)
 chrome_image_label.grid(row=1, column=0, padx=2, pady=2)
 
-#Checkbox
+# Checkbox
 chrome_invar = IntVar()
 chrome_checkbox = Checkbutton(root, text="Chrome", variable=chrome_invar)
 chrome_checkbox.grid(row=1, column=1, padx=2, pady=2)
 
+# Opera
 
-#Opera
 
-
-#Load image
+# Load image
 opera_image = Image.open("./Resources/opera.png")  # Replace "example.png" with your image file path
 opera_image = opera_image.resize((20, 20))  # Resize the image to your desired dimensions
 opera_photo = ImageTk.PhotoImage(opera_image)
 
-#Display image
+# Display image
 opera_image_label = tk.Label(root, image=opera_photo)
 opera_image_label.grid(row=2, column=0, padx=2, pady=2)
 
-#Checkbox
+# Checkbox
 opera_invar = IntVar()
 opera_checkbox = Checkbutton(root, text="Opera", variable=opera_invar)
 opera_checkbox.grid(row=2, column=1, padx=2, pady=2)
 
-
-
-#Button
+# Button
 button = tk.Button(root, text="Install", command=bttn_click)
 button.grid(row=3, column=0, padx=2, pady=2, columnspan=3)
 
-#Main
+# Main
 if __name__ == '__main__':
     root.mainloop()
--- programs.py
+++ programs.py
@@ -1,12 +1,14 @@
 import os
 
-#Class for apps
+
+# Class for apps
 class App:
     def __init__(self, name, url, flags):
         self.name = name
         self.url = url
         self.flags = flags
-        self.path = f"{os.getcwd()}/Installers/{name}Setup.exe"
+        self.path = f"{os.getcwd()}\\Installers\\{name}Setup.exe"
+
 
 chrome = App("Chrome",
              "https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B618ACBDD-3ECC-E2AE-B51C-62A89ADF533C%7D%26lang%3Dhu%26browser%3D4%26usagestats%3D1%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26brand%3DYTUH%26installdataindex%3Dempty/update2/installers/ChromeSetup.exe",
@@ -14,4 +16,4 @@ chrome = App("Chrome",
 
 opera = App("Opera",
             "https://net.geo.opera.com/opera_gx/stable/windows?utm_source=google&utm_medium=ose&utm_campaign=%28none%29&http_referrer=https%3A%2F%2Fwww.google.com%2F&utm_site=opera_com&utm_lastpage=opera.com%2F&dl_token=58304643",
-            ["/silent", "/allusers=1", "/setdefaultbrowser=0", "/pintotaskbar=0"])
+            ["/silent", "/allusers=1", "/setdefaultbrowser=0", "/pintotaskbar=0", "/launchbrowser=1"])
