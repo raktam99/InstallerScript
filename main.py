77ae7ca26201527115176aaf185cf4b1bacc07ca
raktam99 <raktam78@gmail.com>
2023-09-21 21:28:58 +0200

    Bugfix

--- main.py
+++ main.py
@@ -7,6 +7,36 @@ import subprocess
 import programs
 
 
+# Install click handler function
+def download_bttn_click():
+    if not os.path.isdir(programs.installer_path):
+        os.mkdir(programs.installer_path)
+    print("Created directory where installers will be stored!")
+
+    if not len(programs_to_install) == 0:
+        print("Downloading Programs...")
+        for program in programs_to_install:
+            download_app(program)
+
+        print("Downloaded everything!")
+    else:
+        print("Nothing is selected")
+
+
+# Install click handler function
+def install_bttn_click():
+    if not len(programs_to_install) == 0:
+        print("Installing programs...")
+
+        for program in programs_to_install:
+            install_app(program)
+            installed_programs.append(program)
+
+        print("Everything is installed!")
+    else:
+        print("Nothing to install!")
+
+
 # Downloader function
 def download_app(application):
     print(f"{application.name}...")
@@ -43,36 +73,6 @@ def install_app(application):
             print(f"Installer not found at path: {path}")
     else:
         print("Already installed!")
-
-
-# Install click handler function
-def download_bttn_click():
-    if not os.path.isdir(programs.installer_path):
-        os.mkdir(programs.installer_path)
-    print("Created directory where installers will be stored!")
-
-    if not len(programs_to_install) == 0:
-        print("Downloading Programs...")
-        for program in programs_to_install:
-            download_app(program)
-
-        print("Downloaded everything!")
-    else:
-        print("Nothing is selected")
-
-
-# Install click handler function
-def install_bttn_click():
-    if not len(programs_to_install) == 0:
-        print("Installing programs...")
-
-        for program in programs_to_install:
-            install_app(program)
-            installed_programs.append(program)
-
-        print("Everything is installed!")
-    else:
-        print("Nothing to install!")
 
 
 # region Box handlers
--- programs.py
+++ programs.py
@@ -1,9 +1,9 @@
 import os
 
 installer_path = f"{os.path.expanduser('~')}\\Downloads\\Downloaded Installers"
+
+
 # Class for apps
-
-
 class App:
     def __init__(self, name, url, flags):
         self.name = name
